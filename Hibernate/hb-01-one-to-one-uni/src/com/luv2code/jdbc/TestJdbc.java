a98837c75634a987ffd9c3a5809da648451aca36
sugyan84 <sugyansahu84@gmail.com>
2017-12-08 22:02:27 +0530

    Logger implementation.

--- Hibernate/hb-01-one-to-one-uni/src/com/luv2code/jdbc/TestJdbc.java
+++ Hibernate/hb-01-one-to-one-uni/src/com/luv2code/jdbc/TestJdbc.java
@@ -7,35 +7,32 @@ import java.util.logging.Logger;
 public class TestJdbc
 {
 
-	Logger l = Logger.getLogger(this.getClass().getSimpleName());
-	
-	
+	static Logger log = Logger.getLogger(TestJdbc.class.getSimpleName());
+
 	public static void main(String[] args)
 	{
-		
+		log.info("main method");
+
 		String jdbcUrl = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?useSSL=false";
-		
+
 		String username = "hbstudent";
 		String password = "hbstudent";
-		
-		
-		
+
 		try
 		{
 			System.out.println("Connecting to mySQL database...");
-			
-			
-			
+
 			Connection con = DriverManager.getConnection(jdbcUrl, username, password);
-			
-			System.out.println("Connection successful");
-			
-			
-		}catch(Exception e)
+
+			// System.out.println("Connection successful");
+
+			log.info("info");
+
+		}
+		catch(Exception e)
 		{
-			
+
 		}
 	}
 
 }
-
