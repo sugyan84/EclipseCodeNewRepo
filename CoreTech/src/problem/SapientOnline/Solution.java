7cf85ef23e065a5bb67a95158c87222fb0cab99c
sugyan84 <sugyansahu84@gmail.com>
2017-12-10 11:52:19 +0530

    Sapient 2nd Problem final Code.

--- CoreTech/src/problem/SapientOnline/Solution.java
+++ CoreTech/src/problem/SapientOnline/Solution.java
@@ -1,4 +1,4 @@
-package problem.SapientOnline;
+package new3;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -7,13 +7,12 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
-class TestClass
+class Solution
 {
 
 	public static boolean isPrime(int num)
 	{
-		if(num==1)
-			return true;
+		if(num == 1) return false;
 		for(int i = 2; i < num; i++)
 		{
 			if(num % i == 0)
@@ -26,53 +25,180 @@ class TestClass
 		return true;
 	}
 
+	/*
+	 * public static void main(String args[]) throws Exception {
+	 * 
+	 * BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	 * String name = br.readLine();
+	 * 
+	 * List<Integer> nonPrime = new ArrayList<Integer>();
+	 * 
+	 * List<Integer> prime = new ArrayList<Integer>();
+	 * 
+	 * int N = Integer.parseInt(br.readLine().trim());
+	 * 
+	 * String[] strSplit = br.readLine().trim().split(" ");
+	 * 
+	 * for(int i = 0; i < strSplit.length; i++) { int x =
+	 * Integer.parseInt(strSplit[i]);
+	 * 
+	 * if(isPrime(x)) { // prime prime.add(x); } else { nonPrime.add(x); } }
+	 * 
+	 * // prime Collections.sort(prime);
+	 * 
+	 * // desc: nonPrime Collections.sort(nonPrime, Collections.reverseOrder());
+	 * 
+	 * Iterator pitr = prime.iterator();
+	 * 
+	 * while (pitr.hasNext()) { System.out.print(pitr.next() + " "); }
+	 * 
+	 * Iterator npitr = nonPrime.iterator();
+	 * 
+	 * while (npitr.hasNext()) { System.out.print(npitr.next() + " "); }
+	 * 
+	 * }
+	 */
+
+	public static void sortAsc(int[] a)
+	{
+		quickSortAsc(0, a.length - 1, a);
+	}
+
+	public static void quickSortAsc(int start, int end, int[] q)
+	{
+		if(start >= end) return;
+
+		int pIndex = partitionAsc(start, end, q);
+
+		quickSortAsc(start, pIndex - 1, q);
+
+		quickSortAsc(pIndex + 1, end, q);
+
+	}
+
+	public static int partitionAsc(int start, int end, int[] p)
+	{
+		int pIndex = start;
+		int pivot = p[end];
+
+		for(int i = start; i <= end - 1; i++)
+		{
+			if(p[i] <= pivot)
+			{
+				swap(i, pIndex, p);
+				pIndex++;
+			}
+		}
+
+		swap(end, pIndex, p);
+
+		return pIndex;
+
+	}
+
+	public static void sortDsc(int[] a)
+	{
+		quickSortDsc(0, a.length - 1, a);
+	}
+
+	public static void quickSortDsc(int start, int end, int[] q)
+	{
+		if(start >= end) return;
+
+		int pIndex = partitionDsc(start, end, q);
+
+		quickSortDsc(start, pIndex - 1, q);
+
+		quickSortDsc(pIndex + 1, end, q);
+
+	}
+
+	public static int partitionDsc(int start, int end, int[] p)
+	{
+		int pIndex = start;
+		int pivot = p[end];
+
+		for(int i = start; i <= end - 1; i++)
+		{
+			if(p[i] >= pivot)
+			{
+				swap(i, pIndex, p);
+				pIndex++;
+			}
+		}
+
+		swap(end, pIndex, p);
+
+		return pIndex;
+
+	}
+
+	public static void swap(int i, int pIndex, int[] p)
+	{
+		int temp = 0;
+		temp = p[i];
+		p[i] = p[pIndex];
+		p[pIndex] = temp;
+	}
+
 	public static void main(String args[]) throws Exception
 	{
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String name = br.readLine();
-
-		List<Integer> nonPrime = new ArrayList<Integer>();
-
-		List<Integer> prime = new ArrayList<Integer>();
+		//String name = br.readLine();
 
 		int N = Integer.parseInt(br.readLine().trim());
 
 		String[] strSplit = br.readLine().trim().split(" ");
 
+		int[] intArr = new int[N];
+
 		for(int i = 0; i < strSplit.length; i++)
 		{
-			int x = Integer.parseInt(strSplit[i]);
+			intArr[i] = Integer.parseInt(strSplit[i]);
 
-			if(isPrime(x))
-			{
-				// prime
-				prime.add(x);
-			}
-			else
-			{
-				nonPrime.add(x);
-			}
 		}
 
-		// prime
-		Collections.sort(prime);
+		int lastPrime = 0;
 
-		// desc: nonPrime
-		Collections.sort(nonPrime, Collections.reverseOrder());
+		int primeEdge = 0;
 
-		Iterator pitr = prime.iterator();
+		boolean swapped = true;
 
-		while (pitr.hasNext())
+		for(int i = 0; i < intArr.length; i++)
 		{
-			System.out.print(pitr.next() + " ");
+
+			swapped= false;
+			
+			if(!isPrime(intArr[i])) /// nonprime
+			{
+				// nonPrime
+				for(int j = lastPrime + 1; j < N; j++)
+				{
+					if(isPrime(intArr[j]))
+					{
+						// prime
+						swap(i, j, intArr);
+						lastPrime = j;
+						primeEdge = i;
+
+						swapped = true;
+						break;
+
+					}
+				}
+			}
 		}
-
-		Iterator npitr = nonPrime.iterator();
-
-		while (npitr.hasNext())
+		
+		
+		quickSortAsc(0, primeEdge, intArr);
+		
+		quickSortDsc(primeEdge+1, intArr.length-1, intArr);
+		
+		
+		for(int k=0; k<N; k++)
 		{
-			System.out.print(npitr.next() + " ");
+			System.out.println(intArr[k]+" ");
 		}
 
 	}
